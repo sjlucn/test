165e441bde3d8e77300e07aef9ae94924095d2ab
sjlucn <sjlucn@sjlucn-PC>
2015-10-03 22:12:43 +0800

    test

---
+++ Test/src/com/test/T.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class T {
+
+	public T() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hi");
+
+	}
+
+}
